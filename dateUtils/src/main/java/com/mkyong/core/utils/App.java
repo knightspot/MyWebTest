e54efb535214dc4087c5effbe40e5affe2b493cb
Jonas Petersson <jonasp@telia.com>
2016-05-21 17:21:49 +0200

    Initial commit

---
+++ dateUtils/src/main/java/com/mkyong/core/utils/App.java
@@ -0,0 +1,39 @@
+package com.mkyong.core.utils;
+
+import org.joda.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.knightspot.HelloWorld;
+
+public class App {
+
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+
+		System.out.println(getLocalCurrentDate());
+		
+		ApplicationContext context = 
+	             new ClassPathXmlApplicationContext("Beans.xml");
+
+	      HelloWorld obj = (HelloWorld) context.getBean("first");
+
+	      logger.info( "I have got the message:"+ obj.getMessage());
+	      
+	}
+
+	private static String getLocalCurrentDate() {
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("getLocalCurrentDate() is executed!");
+		}
+
+		LocalDate date = new LocalDate();
+		return date.toString();
+
+	}
+
+}
